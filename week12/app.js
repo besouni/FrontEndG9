19882c213196af9946ee3fa5583af8f653bd994d
Beso Tabatadze <besotabatadze84@gmail.com>
2021-06-14 16:48:32 +0400

    Lecture 12

---
+++ week12/app.js
@@ -0,0 +1,27 @@
+function f1(){
+   console.log("BODY");
+}
+
+function f2(){
+   console.log("DIV");
+}
+
+function f3(){
+   console.log("FORM");
+}
+
+function f4(){
+   console.log("IMG");
+}
+
+function f5(e){
+   console.log("DOWN->");
+   console.log(e);
+   console.log(e.keyCode);
+}
+
+function f6(e){
+   console.log("PRESS->");
+   console.log(e);
+   console.log(e.keyCode);
+}
---
+++ week12/home/app.js
@@ -0,0 +1,10 @@
+// var send = document.getElementById("send");
+// send.addEventListener("click", function(event){
+//    event.preventDefault();
+//    console.log(event);
+// })
+
+function f(e){
+   e.preventDefault();
+   console.log(e);
+}
